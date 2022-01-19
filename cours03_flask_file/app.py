0f293ba9a80f6162449ba22ef7d1ffe03e0820be
kbenmus02 <mkbensmaia@gmail.com>
2022-01-18 19:26:29 -0500

    fix requirements.txt

--- cours03_flask_file/app.py
+++ cours03_flask_file/app.py
@@ -190,4 +190,5 @@ def load_model(model_file_info: str):
     #--- Charge le modèle
 
 if __name__ == "__main__":
-    app.run()
+    port = os.environ.get("PORT", 5000)  
+    app.run(debug=False, host="0.0.0.0", port = port)
